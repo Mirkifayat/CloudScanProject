d637f97c12fb5de7046707ec6826a40f72ab2db5
Mirkifayat <mirkifayat292@gmail.com>
2026-04-18 18:45:06 +0530

    Integrated fast-scan flags with default NSE scripts

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def scan():
         
         # Using -F (Fast), -Pn (No Ping), and --script=vuln for Cyber depth
        # Added -T4 for speed and -max-retries to stop Nmap from trying too hard on blocked ports
-        command = [nmap_path, "-sT", "-F", "-Pn", "-sV", "-T4", "--max-retries", "1", target]
+        command = ["nmap", "-sT", "-F", "-Pn", "-sV", "-T4", "--max-retries", "1", "--script=vuln", target]
         
         # Timeout set to 150 seconds because 'vuln' scripts take time
         result = subprocess.run(command, capture_output=True, text=True, timeout=150)
